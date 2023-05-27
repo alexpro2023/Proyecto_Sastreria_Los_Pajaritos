77109099a5fe5b888084a876d4b78aa9f7abfb15
alexpro2023 <100328908+alexpro2023@users.noreply.github.com>
2023-05-27 05:31:59 -0600

    bordados_App_backend, master

---
+++ bordados_app_backend/api/config/database.php
@@ -0,0 +1,23 @@
+<?php
+class Conexion
+{
+    public $conn;
+    public $database_name = "LosPajaritos.";
+    public $col_users = "users";
+    public $col_categories = "categories";
+    public $col_products = "products";
+    public $col_dumps = "dumps";
+    public $col_restorations = "restorations";
+
+    public function conectar()
+    {
+        try {
+            $cadenaConexion = "mongodb+srv://sastrerialospajaritos:fUIRMRzwPDAtkV9u@cluster43000.2ddxg0t.mongodb.net/?retryWrites=true&w=majority";
+            $cliente = new MongoDB\Driver\Manager($cadenaConexion);
+            return $cliente;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+}
+?>
